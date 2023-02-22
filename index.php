cf5dbb048dbe5adb2f1a8f43041fab62a63e660f
Ilya-Neverov <102865207+Ilya-Neverov@users.noreply.github.com>
2023-02-22 21:58:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,17 +9,15 @@
         <h1>Калькулятор роста</h1>
         <section>
             <?php
-                if (array_key_exists('int', $_POST)) {
-                    if (preg_match_all("/[1-2][0-9][0-9]/", $_POST['int'])) {
-                        echo 'Ваш рост ' . "$_POST[int]" . ' см !!!!';
-                        echo '<p>пересчитать Ваш рост?</p>';
-                    } else {
-                        echo '<p>некоретный введен рост в сантиметрах</p>';
-                    }
+            if (array_key_exists('int', $_POST)) {
+                if ($_POST['int'] < '288' && $_POST['int'] > '40') {
+                    echo 'Ваш рост ' . "$_POST[int]" . ' см.<br> Пересчитать рост?</p>';
                 } else {
-
-                    echo '<p>введите Ваш рост в сантиметрах</p>';
+                    echo '<p>Некорректно введен рост, Вы ввели <mark>' . "$_POST[int]" . '</mark><br> Попробуйте еще раз =)</p>';
                 }
+            } else {
+                echo '<p>Введите Ваш рост в см.</p>';
+            }
             ?>
             <form action="index.php" name="height" method="POST">
                 <input name="int" type="text">
